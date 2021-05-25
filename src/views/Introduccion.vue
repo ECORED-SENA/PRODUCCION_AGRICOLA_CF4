a36cbd5b38041bf5985480585c8ce16c7e7ee536
scamacho84 <sergio.camachoo@misena.edu.co>
2021-05-25 18:03:47 -0500

    Correccion 4

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -13,10 +13,7 @@
       .video
         iframe(width="560" height="315" src="https://www.youtube.com/embed/4ILONVFMai0" title="Introduccion" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
-    p.mb-5 La auditoría presenta gran relevancia para una empresa, ya que esta surge como respuesta de control y vigilancia de los procesos, procedimientos y actividades ejecutados por la entidad, convirtiéndose en una herramienta de prevención que ayuda al correcto cumplimiento de los objetivos y metas de las empresas, minimizando el riesgo del mal uso de los recursos o de su pérdida absoluta.
-      br
-      br
-      | En la implementación de Buenas Prácticas Agrícolas y en la mayoría de los sistemas de aseguramiento de calidad hay que adelantar actividades de auditoría, que ayuden a establecer el grado de cumplimiento frente a los estándares normativos para poder obtener una certificación.
+    p.mb-5 La auditoría presenta gran relevancia para una empresa, ya que esta surge como respuesta de control y vigilancia de los procesos, procedimientos y actividades ejecutados por la entidad, convirtiéndose en una herramienta de prevención que ayuda al correcto cumplimiento de los objetivos y metas de las empresas, minimizando el riesgo del mal uso de los recursos o de su pérdida absoluta. En este material se presenta el concepto de auditoría, los tipos de auditoría, la metodología para su realización y la aplicación de la auditoría en una empresa específica.
 
 </template>
 
