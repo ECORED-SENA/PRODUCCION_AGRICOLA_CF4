99cecf977dea4a953d6aec27cf0144882f9fa276
scamacho84 <sergio.camachoo@misena.edu.co>
2021-05-27 15:24:20 -0500

    Correcciones 7

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -11,7 +11,7 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/4ILONVFMai0" title="Introduccion" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/bGIAk8dI4z0" title="Introduccion" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     p.mb-5 La auditoría presenta gran relevancia para una empresa, ya que esta surge como respuesta de control y vigilancia de los procesos, procedimientos y actividades ejecutados por la entidad, convirtiéndose en una herramienta de prevención que ayuda al correcto cumplimiento de los objetivos y metas de las empresas, minimizando el riesgo del mal uso de los recursos o de su pérdida absoluta. En este material se presenta el concepto de auditoría, los tipos de auditoría, la metodología para su realización y la aplicación de la auditoría en una empresa específica.
 
